8e2a85ff7b49c9fd9aeb85f9f27102aaf2f029f0
Matthew Marcellino <mattmarclj@gmail.com>
2024-01-10 00:40:04 +0700

    update

--- g4f/Provider/Bing.py
+++ g4f/Provider/Bing.py
@@ -42,7 +42,7 @@ class Bing(AsyncGeneratorProvider):
         messages: Messages,
         proxy: str = None,
         cookies: dict = None,
-        tone: str = Tones.creative,
+        tone: str = Tones.precise,
         image: str = None,
         web_search: bool = False,
         **kwargs
